51a140f52a93cae2776462cf04aadcb70372e7bd
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-02-17 23:17:09 +0600

    added login-form-html- and-log-in-js

---
+++ script/log-in.js
@@ -0,0 +1,17 @@
+document.getElementById('login-btn')
+.addEventListener('click', function(){
+  const inputNumber = document.getElementById('input-number')
+  const contactNumber = inputNumber.value
+  
+  const inputPin = document.getElementById('input-pin')
+  const pin = inputPin.value
+  if(contactNumber === '11111111111' && pin === '1111'){
+    alert('Log in Successfully')
+    window.location.assign('/homes.html')
+  }
+  else{
+    alert('Log in Failed')
+    return;
+  }
+  
+})
